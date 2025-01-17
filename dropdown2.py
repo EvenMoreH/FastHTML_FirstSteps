4d7f440e45f06f4087be57094c9fb6904e3e52f9
EvenMoreH <hubert.lewan@gmail.com>
2025-01-17 21:30:56 +0100

    button works -polishing required

--- dropdown2.py
+++ dropdown2.py
@@ -18,7 +18,7 @@ app, rt = fast_app(static_path="app/static") # type: ignore
 def homepage():
     return Html(
         Head(
-            Title("Fast Tools Hub"),
+            Title("Dropdown testing"),
             Meta(name="viewport", content="width=device-width, initial-scale=1"),
             Script(src="https://unpkg.com/htmx.org"),
             Link(rel="stylesheet", href="styles.css"),
@@ -29,34 +29,44 @@ def homepage():
             Div(
                 Button(
                     "Dropdown",
+                    id="dropdown-button",
                     hx_get="/show",
-                    hx_target="#target",
+                    hx_target="#dropdown-menu",
                     style="width: max(200px, 20%)",
                 ),
             ),
             Div(
-                id="target",
+                id="dropdown-menu",
                 style="display: flex; flex-direction: column;"
             ),
-            Div("TEXT TO SEE IF MY CONTENT WILL MOVE"),
-            Div("TEXT TO SEE IF MY CONTENT WILL MOVE"),
-            Div("TEXT TO SEE IF MY CONTENT WILL MOVE"),
-            Div("TEXT TO SEE IF MY CONTENT WILL MOVE"),
-            Div("TEXT TO SEE IF MY CONTENT WILL MOVE"),
-            Div("TEXT TO SEE IF MY CONTENT WILL MOVE"),
+            Div(style="padding: 1rem"),
+            Div("TEXT TO SEE IF CONTENT WILL MOVE"),
+            Div("TEXT TO SEE IF CONTENT WILL MOVE"),
+            Div("TEXT TO SEE IF CONTENT WILL MOVE"),
+            Div("TEXT TO SEE IF CONTENT WILL MOVE"),
+            Div("TEXT TO SEE IF CONTENT WILL MOVE"),
+            Div("TEXT TO SEE IF CONTENT WILL MOVE"),
+            Div(style="padding: 1rem"),
+            Button(
+                "Next",
+                hx_get="/output/{option}",
+                hx_target="body",
+                type="submit",
+            style="width: max(200px, 20%)",
+            )
         )
     )
 
 @rt("/show")
 def show():
     return Div(
-                A("A"),
-                A("B"),
-                A("C"),
-                id="target",
+                Button("A", hx_target="#dropdown-button", hx_get="/selected/A", hx_trigger="click", hx_swap="outerHTML"),
+                Button("B", hx_target="#dropdown-button", hx_get="/selected/B", hx_trigger="click", hx_swap="outerHTML"),
+                Button("C", hx_target="#dropdown-button", hx_get="/selected/C", hx_trigger="click", hx_swap="outerHTML"),
+                id="dropdown-menu",
                 style="display: flex; flex-direction: column; text-align: center; background: grey; width: max(200px, 20%); position: absolute;",
                 # mouseleave trigger to hide content when mouse moves out of list of dropdown items
-                hx_trigger="mouseleave delay:250ms",
+                hx_trigger="mouseleave delay:200ms",
                 hx_get="/hide",
             )
 
@@ -64,8 +74,50 @@ def show():
 def hide():
     return Div(
                 "",
-                id="target",
+                id="dropdown-menu",
                 style="display: flex; flex-direction: column;"
             )
+
+@rt("/selected/{option}")
+def selected(session, option: str):
+    session["chosen_type"] = option
+
+    return Button(
+        f"Option {option}",
+        id="dropdown-button",
+        hx_get="/show",
+        hx_target="#dropdown-menu",
+        hx_trigger="click",
+        style="width: max(200px, 20%)",
+        )
+
+@rt("/output/{option}")
+async def output(session, option:str):
+    chosen_type = session.get("chosen_type", "No value set")
+    if chosen_type == "A":
+        item_type = "Armor"
+    elif chosen_type == "B":
+        item_type = "Weapon"
+    else:
+        item_type = "Misc"
+
+    return Html(
+        Head(
+            Title("Dropdown testing"),
+            Meta(name="viewport", content="width=device-width, initial-scale=1"),
+            Script(src="https://unpkg.com/htmx.org"),
+            Link(rel="stylesheet", href="styles.css"),
+            Link(rel="icon", href="images/favicon.ico", type="image/x-icon"),
+            Link(rel="icon", href="images/favicon.png", type="image/png"),
+        ),
+        Body(
+            Button(
+                f"Item type selected: {item_type} (Click to return)",
+                hx_get="/",
+                hx_target="body",
+                style="width: max(200px, 20%)",
+            )
+        )
+    )
 
 serve()
